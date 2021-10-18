5fb5b916061d7e58c78ba9d9f43a2e78c7f53a08
吴操 <wucao@qiyi.com>
2021-10-18 18:23:50 +0800

    新增握手处理

--- src/main/java/com/xxg/network/netty/websocket/WebSocketServer.java
+++ src/main/java/com/xxg/network/netty/websocket/WebSocketServer.java
@@ -5,6 +5,7 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
@@ -44,10 +45,21 @@ public class WebSocketServer {
 class MyWebSocketServerHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) {
         if (frame instanceof TextWebSocketFrame) {
             String request = ((TextWebSocketFrame) frame).text();
             ctx.channel().writeAndFlush(new TextWebSocketFrame("收到: " + request));
+        }
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+        if (evt instanceof WebSocketServerProtocolHandler.HandshakeComplete) {
+            WebSocketServerProtocolHandler.HandshakeComplete handshakeCompletedEvent = (WebSocketServerProtocolHandler.HandshakeComplete) evt;
+            String uri = handshakeCompletedEvent.requestUri(); // 握手请求 URI
+            HttpHeaders headers = handshakeCompletedEvent.requestHeaders(); // 握手请求头
+
+            // TODO 这里可以获取握手请求 URL 中的参数，或对用户进行登录认证
         }
     }
 }
